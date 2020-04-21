1a5d6b82c58ae3056d2faf4fbbf7de96bd7e21ae
Janne Rättyä <janne.rattya@web-veistamo.fi>
2020-04-21 18:29:17 +0300

    Switch to scrollmagic

--- myssyfarmi-lampaat/assets/js/scripts.js
+++ myssyfarmi-lampaat/assets/js/scripts.js
@@ -1,4 +1,4 @@
-var mySwiper = new Swiper('.swiper-container', {
+/*var mySwiper = new Swiper('.swiper-container', {
     cssMode: true,
     direction: 'vertical',
     loop: false,
@@ -11,4 +11,26 @@ var mySwiper = new Swiper('.swiper-container', {
     mousewheelSensitivity: 0,
     mousewheelReleaseOnEdges: true,
     touchReleaseOnEdges: true,
-})
+})*/
+
+
+var controller = new ScrollMagic.Controller({
+    globalSceneOptions: {
+        triggerHook: 'onLeave',
+        duration: "100%",
+    }
+});
+
+// get all slides
+var slides = document.querySelectorAll(".swiper-slide");
+
+// create scene for every slide
+for (var i = 0; i < slides.length; i++) {
+    new ScrollMagic.Scene({
+        triggerElement: slides[i]
+    })
+        .setPin(slides[i], { pushFollowers: false })
+        //.setClassToggle(slides[i], "swiper-pagination-bullet-active")
+        //.addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+}
